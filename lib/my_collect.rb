46ecdf7d6fbda3b2a852986381a2c22726223568
claire <clairebontempo@gmail.com>
2020-10-23 11:45:29 -0700

    commit tree c22e5d66dbc626d308da1a32afcdd0d82e248271

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+def my_collect(collection)
+    i = 0
+    new_collection = []
+    while i < collection.length
+        new_collection << yield(collection[i])
+        i += 1
+    end
+    new_collection
+end
 
+
+    #iterate over collection using while loop
+    #execute code in block you call it with, for each element
+    #using yield
+    #return modified collection
 
